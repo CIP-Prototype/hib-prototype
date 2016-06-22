7b6bfdac3214cce81191596b996b38fd445a30ba
Steve Powell <steve.reloadcom@gmail.com>
2016-06-22 12:55:05 +0100

    hib prototype upload

---
+++ js/diabetes_start.js
@@ -0,0 +1,13 @@
+//Waits for page to load
+$(window).load(function(){
+	//Triggers when sign in button is clicked
+	$("#signIn").click(function (){
+		//sets and stores the app name to pass it to the choser/sign in pages
+		var app = "diabetes";
+		sessionStorage.clear();
+		sessionStorage.app = app;
+
+		//directs the user to the IDP chooser page
+		window.location.href = "chooser.html";
+	});
+});
