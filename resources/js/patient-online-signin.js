8b49fad3f8ab462b9152a370c235da6ff7d5c753
webby980 <alistair.webster@nhs.net>
2016-06-30 14:33:21 +0100

    Edited patient online signin page, added JS for the page as well as diabetes landing page

---
+++ resources/js/patient-online-signin.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+    
+	$("#submit").click(patientLogin);
+});
+
+function patientLogin () {   
+    if (sessionStorage.app == 'bookGP') {
+        window.location.href = 'appointment-complete.html';
+    }
+    else if (sessionStorage.app == 'orderPresc'){
+        window.location.href = 'prescriptions-complete.html';
+    }
+    else if (sessionStorage.app == 'viewPlanner'){
+        window.location.href = 'diabetes-complete.html';
+    }
+};
