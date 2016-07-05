ccfa8e9ab3904581bfbc04a167b925a4e9ec4268
webby980 <alistair.webster@nhs.net>
2016-07-05 13:48:11 +0100

    Added visit-gp page

--- resources/js/verify-signin.js
+++ resources/js/verify-signin.js
@@ -9,15 +9,15 @@ function verifySignin () {
         window.location.href = 'appointment-complete.html';     
     }
     else if (sessionStorage.app == 'orderPresc'){
-        sessionStorage.loggedIn = true;
+        sessionStorage.loggedIn = 'true';
         window.location.href = 'prescriptions-complete.html';
     }
     else if (sessionStorage.app == 'viewPlanner'){
-        sessionStorage.loggedIn = true;
+        sessionStorage.loggedIn = 'true';
         window.location.href = 'diabetes-complete.html';
     }
     else if (sessionStorage.app == 'yourApp'){
-        sessionStorage.loggedIn = true;
+        sessionStorage.loggedIn = 'true';
         window.location.href = 'your-app-biometric.html';
     }
 };
