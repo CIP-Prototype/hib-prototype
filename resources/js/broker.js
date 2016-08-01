64a6dbd2df8b03ccba5e25e6006831773ee41e7a
amitn <Amit.Nagmode@mastek.com>
2016-08-01 12:43:13 +0100

    update to reflect chnages to tech demo

--- resources/js/broker.js
+++ resources/js/broker.js
@@ -2,20 +2,17 @@ var radioOption = "";
 
 $(window).load(function() {
 
-	$("#yes").click({radioOption:"yes"}, radioSelect);
-	$("#create").click({radioOption:"create"},radioSelect);
-	$("#continue").click({radioOption:"continue"},radioSelect);
 	$("#Next").click(radioEnter);
 	$("#registerHere").click(showCreatePage);
-	
+
 	if (sessionStorage.app == "orderPresc" || sessionStorage.app == "viewPlanner") {
 		$("#noLogIn").hide();
 	}
-	
+
 	if (sessionStorage.app == "orderPresc") {
 		$("#verify").hide();
-	}	
-	
+	}
+
 });
 
 function radioSelect (option) {
@@ -24,34 +21,21 @@ function radioSelect (option) {
 
 function radioEnter () {
 
-				if( radioOption == 'yes') {
-					$("#chooser").hide();
-					$("#createAccount").hide();
-					$("#selector").show();				
-				}
-				else if (radioOption == 'create'){
-					$("#chooser").hide();
-					$("#selector").hide();
-					$("#createAccount").show();
-				}
-			 	else if (radioOption == 'continue'){
-						if (sessionStorage.app == "bookGP" ) {
-							window.location.href = 'appointment-complete.html'
-						}
-						else if (sessionStorage.app == "orderPresc" ) {
-							window.location.href = 'prescriptions-complete.html'
-						}
-						else if (sessionStorage.app == "viewPlanner" ) {
-							window.location.href = 'diabetes-complete.html'
-						}
-						else if (sessionStorage.app == "yourApp" ) {
-							window.location.href = 'your-app-biometric.html'
-						}
-				}
+	if (sessionStorage.app == "bookGP" ) {
+		window.location.href = 'appointment-complete.html'
+	}
+	else if (sessionStorage.app == "orderPresc" ) {
+		window.location.href = 'prescriptions-complete.html'
+	}
+	else if (sessionStorage.app == "viewPlanner" ) {
+		window.location.href = 'diabetes-complete.html'
+	}
+	else if (sessionStorage.app == "yourApp" ) {
+		window.location.href = 'your-app-biometric.html'
+	}
 };
 
 function showCreatePage () {
-	$("#chooser").hide();
-	$("#selector").hide();
 	$("#createAccount").show();
+	$("#selector").hide();
 };
